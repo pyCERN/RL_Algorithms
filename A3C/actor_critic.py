4c2ae66701396d75c7e4e83605b2dd30ff763429
pycern <39230856+pyCERN@users.noreply.github.com>
2021-10-08 00:39:18 +0900

    Updating A3C

--- A3C/actor_critic.py
+++ A3C/actor_critic.py
@@ -3,32 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Actor(nn.Module):
+class ActorCritic(nn.Module):
     def __init__(self, n_in, n_out):
-        super(Actor, self).__init__()
+        super(ActorCritic, self).__init__()
         self.fc1 = nn.Linear(n_in, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, n_out)
+        self.fc3_1 = nn.Linear(64, n_out)
+        self.fc3_2 = nn.Linear(64, 1)
 
     def forward(self, x):
         h1 = F.relu(self.fc1(x))
         h2 = F.relu(self.fc2(h1))
-        prob = self.fc3(h2)
+        prob = self.fc3_1(h2)
         prob = F.softmax(prob, dim=1)
+        v_value = self.fc3_2(h2)
 
-        return prob
+        return prob, v_value
 
-class Critic(nn.Module):
-    def __init__(self, n_in):
-        super(Critic, self).__init__()
-        self.fc1 = nn.Linear(n_in, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 1)
+# class Critic(nn.Module):
+#     def __init__(self, n_in):
+#         super(Critic, self).__init__()
+#         self.fc1 = nn.Linear(n_in, 64)
+#         self.fc2 = nn.Linear(64, 64)
+#         self.fc3 = nn.Linear(64, 1)
 
-    def forward(self, x):
-        h1 = F.relu(self.fc1(x))
-        h2 = F.relu(self.fc2(h1))
-        v_value = self.fc3(h2)
+#     def forward(self, x):
+#         h1 = F.relu(self.fc1(x))
+#         h2 = F.relu(self.fc2(h1))
+#         v_value = self.fc3(h2)
 
-        return v_value
+#         return v_value
         
--- A3C/main.py
+++ A3C/main.py
@@ -1,10 +1,7 @@
 import argparse
+import gym
 
-import torch
-import torch.nn as nn
-
-from agent import Agent
-from env import Environment
+from agent import Agent, Worker
 
 '''
 Gradient Parallelism
@@ -14,10 +11,16 @@ parser = argparse.ArgumentParser(description="a2c")
 parser.add_argument("--gamma", type=float, default=0.99, help="discount factor")
 parser.add_argument("--max_steps", type=int, default=500, help="maximum steps")
 parser.add_argument("--max_eps", type=int, default=10000, help="maximum episodes")
-parser.add_argument("--lr_actor", type=float, default=1e-4, help="actor net lr")
-parser.add_argument("--lr_critic", type=float, default=1e-3, help="critic net lr")
+parser.add_argument("--lr_ac", type=float, default=1e-4, help="actor_critic net lr")
+# parser.add_argument("--lr_critic", type=float, default=1e-3, help="critic net lr")
 parser.add_argument("--env", type=str, default="CartPole-v1", help="gym environment")
 args = parser.parse_args()
 
-env = Environment(args)
-env.run()
+num_workers = 2
+env = gym.make(args.env)
+global_net = Agent(args, env)
+workers = [Worker(args, env, global_net) for _ in range(num_workers)]
+
+for worker in workers:
+    worker.start()
+    worker.join()
